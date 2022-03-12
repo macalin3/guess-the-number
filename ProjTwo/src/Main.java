dd93f9644424ac49d5eaf6c185f720dc4c83fb61
macalin3 <macalin3.dm@gmail.com>
2022-03-12 09:59:26 -0700

    initial commit

---
+++ ProjTwo/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        char anotherGame;
+        Scanner sca = new Scanner(System.in);
+
+        do {
+            Main.guessNumber();
+            System.out.println("Would you like to play again? (Y/N)");
+            anotherGame = sca.nextLine().charAt(0);
+        } while (anotherGame == 'y');
+    }
+
+    public static void guessNumber(){
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("Hello! What is your name?");
+        String name = sc.next();
+        System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
+
+        int guess;
+        int randNum = rand.nextInt(20-1) +1;
+        int counter = 0;
+
+        do {
+            System.out.println("Take a guess.");
+            guess = sc.nextInt();
+            counter++;
+
+            if (guess == randNum){
+                System.out.println("Good job, " + name + "! You guessed my number!");
+                System.out.println("You guessed it in " + counter + " tries!");
+            }	else if (randNum < guess){
+                System.out.println("Your guess is too high.");
+            } else if (randNum > guess) {
+                System.out.println("Your guess is too low.");
+            }
+        } while (guess != randNum && counter < 6);
+
+    }
+}
---
+++ README.md.txt
@@ -0,0 +1,3 @@
+#guess-the-number
+
+This is a Java assignment that takes in user input to guess a random number from 1-20. It gives you the option to start over if you guess the number correctly within 6 tries.
